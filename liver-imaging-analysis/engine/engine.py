7d05b75e68d0cd47cc14ce7f737515403a380431
Baraa661 <tarekbaraa7@gmail.com>
2022-12-11 20:35:00 +0200

    Update engine.py

--- liver-imaging-analysis/engine/engine.py
+++ liver-imaging-analysis/engine/engine.py
@@ -6,8 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-
 class Engine(nn.Module):
     """Class that implements the basic PyTorch methods for neural network
     Neural Networks should inherit from this class
@@ -32,16 +30,16 @@ class Engine(nn.Module):
 
         Parameters
         ----------
-        training_data_path: str,
-              String containing paths of the training volumes at the folder Path and
-              masks at the folder Path2.
-        testing_data_path: str,
-              String containing paths of the testing volumes at the folder Path and
-              masks at the folder Path2. 
-        transformation_flag: bool,
+        training_data_path: str
+              String containing paths of the training volumes at the folder
+              Path and masks at the folder Path2.
+        testing_data_path: str
+              String containing paths of the testing volumes at the folder
+              Path and masks at the folder Path2.
+        transformation_flag: bool
               indicates if data preprocessing should be performed or not
               False will ignore "transformation" argument
-        transformation: array_like,
+        transformation: array_like
               an array of the shape data will be transformed into.
               eg; [64,512,512]
         batchsize: int
@@ -79,7 +77,7 @@ class Engine(nn.Module):
         )
         self.train_dataloader = trainloader.get_training_data()
         self.val_dataloader = trainloader.get_testing_data()
-        self.test_dataloader=testloader.get_training_data()
+        self.test_dataloader = testloader.get_training_data()
 
     def data_status(self):
         """
@@ -106,13 +104,30 @@ class Engine(nn.Module):
                 f" {batch['label'].shape} {batch['label'].dtype}"
             )
             break
-        
-    def save_checkpoint(self,path):
+
+    def save_checkpoint(self, path):
+        """
+        Saves current checkpoint to a specific path
+
+        Parameters
+        ----------
+        path: int
+            The path where the checkpoint will be saved at
+        """
         torch.save(self.state_dict(), path)
 
+    def load_checkpoint(self, path):
+        """
+        Loads checkpoint from a specific path
 
-    def load_checkpoint(self,path):
-        self.load_state_dict(torch.load(path,map_location=torch.device('cpu'))) # if working with CUDA remove torch.device('cpu')
+        Parameters
+        ----------
+        path: int
+            The path of the checkpoint
+        """
+        self.load_state_dict(
+            torch.load(path, map_location=torch.device("cpu"))
+        )  # if working with CUDA remove torch.device('cpu')
 
     def compile(self, loss, optimizer, metrics=["loss"]):
         """
@@ -154,17 +169,16 @@ class Engine(nn.Module):
              the number of iterations for fitting. (Default = 1)
         """
         self.epochs = epochs
-        self.total_epochs_loss=[]
+        self.total_epochs_loss = []
         tb = SummaryWriter()
         for epoch in range(epochs):
             print(f"Epoch {epoch+1}\n-------------------------------")
-            epoch_loss=0
+            epoch_loss = 0
             size = self.train_dataloader.__len__()
             self.train()  # from pytorch
             for batch_num, batch in enumerate(self.train_dataloader):
-                volume, mask = \
-                    batch["image"].to(self.device),\
-                    batch["label"].to(self.device)
+                volume, mask = batch["image"].to(self.device),\
+                               batch["label"].to(self.device)
                 if self.expand_flag:
                     volume = volume.expand(
                         1,
@@ -186,22 +200,22 @@ class Engine(nn.Module):
                 # Print Progress
                 current = batch_num * len(volume) + 1
                 if "loss" in self.metrics:
-                    print(f"loss: {loss.item():>7f}  "
+                    print(f"loss: {loss.item():>7f}"
                           f"      [{current:>5d}/{size:>5d}]")
-                    epoch_loss=epoch_loss+loss.item()
-                          
+                    epoch_loss = epoch_loss + loss.item()
+
                 if "dice_score" in self.metrics:
                     print(
                         f"Dice Score: "
                         f"{(1-loss.item()):>7f}  [{current:>5d}/{size:>5d}]"
                     )
-            epoch_loss=epoch_loss/len(self.train_dataloader)
+            epoch_loss = epoch_loss / len(self.train_dataloader)
             self.total_epochs_loss.append(epoch_loss)
             # print(" TOTAL LOSS = ",self.totalloss)
             tb.add_scalar("Epoch average loss", epoch_loss, epoch)
-            if(epoch==0):
+            if epoch == 0:
                 self.save_checkpoint("First_epoch")
-            elif(self.total_epochs_loss[epoch]<self.total_epochs_loss[epoch-1]):
+            elif self.total_epochs_loss[epoch] < self.total_epochs_loss[epoch - 1]:
                 self.save_checkpoint("Best_epoch")
                 # if 'accuracy' in self.Metrics:
                 #     self.eval()
@@ -226,9 +240,8 @@ class Engine(nn.Module):
         test_loss = 0
         with torch.no_grad():
             for batch in dataloader:
-                volume, mask = \
-                    batch["image"].to(self.device),\
-                    batch["label"].to(self.device)
+                volume, mask = batch["image"].to(self.device),\
+                               batch["label"].to(self.device)
                 if self.expand_flag:
                     volume = volume.expand(
                         1,
@@ -243,8 +256,7 @@ class Engine(nn.Module):
                     )
                 pred = self(volume)
                 if "loss" or "dice_score" in self.metrics:
-                    test_loss += \
-                        self.loss(pred, mask).item()
+                    test_loss += self.loss(pred, mask).item()
         test_loss /= num_batches
         if "loss" in self.metrics:
             print(f"loss: {test_loss:>7f}")
@@ -257,8 +269,8 @@ class Engine(nn.Module):
         training dataset by calling "Test"
         """
         self.test(self.train_dataloader)
-        epochs=np.arange(0,self.Epochs)
-        plt.plot(epochs,self.total_epochs_loss)
+        epochs = np.arange(0, self.Epochs)
+        plt.plot(epochs, self.total_epochs_loss)
         plt.show()
 
     def evaluate_test(self):
@@ -283,8 +295,8 @@ class Engine(nn.Module):
         """
         dict_loader = dataloader.LoadImageD(keys=("image", "label"))
         data_dict = dict_loader({"image": volume_path, "label": volume_path})
-        preprocess = dataloader.Preprocessing(
-            ("image", "label"), self.transformation)
+        preprocess = dataloader.Preprocessing(("image", "label"),
+                                              self.transformation)
         data_dict_processed = preprocess(data_dict)
         volume = data_dict_processed["image"]
         volume = volume.expand(
